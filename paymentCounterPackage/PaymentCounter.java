74db051847161c4d81d0dcd5e8729e19568a1e7e
Santiago Schumacher <santiagoschumacher@hotmail.com>
2015-01-15 10:18:34 -0300

    topic 1 payment counter santiagos

---
+++ paymentCounterPackage/PaymentCounter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paymentCounterPackage;
+
+/**
+ *
+ * @author Santiago
+ */
+public class PaymentCounter {
+    
+    public static synchronized PaymentCounter getInstance(){
+        if (PaymentCounter.INSTANCE == null){
+            PaymentCounter.INSTANCE = new PaymentCounter(1);
+        }
+        return PaymentCounter.INSTANCE;
+
+    }
+
+    private int idValue;
+    private static PaymentCounter INSTANCE;
+    
+    private PaymentCounter(final int idValue){
+        this.idValue = idValue;
+    }
+    public synchronized int getNextValue(){
+        return idValue++;
+    }
+
+}
